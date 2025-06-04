ec85bcf984f1f9f5c3adaad06705d61f7630b0e2
masu314 <masu314@example.com>
2025-06-04 22:20:04 +0900

    アプリがクラッシュするエラーを解消するため、MemoAddActivityを作り直す

--- app/src/main/java/com/example/memo/MemoAddActivity.java
+++ app/src/main/java/com/example/memo/MemoAddActivity.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +14,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MemoAddActivity extends AppCompatActivity{
+public class MemoAddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_memo_add);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tvActivity2), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -39,7 +40,7 @@ public class MemoAddActivity extends AppCompatActivity{
     //メニューボタンを押したときの反応を定義する
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        this.finish();
+        finish();
         return super.onOptionsItemSelected(item);
     }
 }
