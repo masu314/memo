7cccead7099eeb22fab2afe3a148fe06d88fd81f
masu314 <masu314@example.com>
2025-06-04 21:02:28 +0900

    アクションバーのaddボタンから画面遷移できるようにする

--- app/src/main/java/com/example/memo/MemoAddActivity.java
+++ app/src/main/java/com/example/memo/MemoAddActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,14 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MemoAddActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_memo_add);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tvActivity2), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -40,12 +39,7 @@ public class MainActivity extends AppCompatActivity {
     //メニューボタンを押したときの反応を定義する
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-        if(id == R.id.action_button){
-            Intent intent1 = new Intent(MainActivity.this, MemoAddActivity.class);
-            startActivity(intent1);
-        }
+        this.finish();
         return super.onOptionsItemSelected(item);
     }
 }
