d515a2d5933920820008d696394b21d43ebd9323
masu314 <masu314@example.com>
2025-07-23 20:54:47 +0900

    [add]アクションバーに戻るボタンを設定 DetailActivityに戻るボタンを表示させる処理と、ボタンを押したときの処理（メイン画面に戻る）を追加 dbHelperのインスタンス化は現在使っていないため削除

--- app/src/main/java/com/example/memo/DetailActivity.java
+++ app/src/main/java/com/example/memo/DetailActivity.java
@@ -1,16 +1,16 @@
 package com.example.memo;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -29,12 +29,15 @@ public class DetailActivity extends AppCompatActivity {
             return insets;
         });
 
-        dbHelper = new DatabaseHelper(this);
+        // アクションバーを取得
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true); // 戻るボタンを表示
+        }
 
         // UIコンポーネントの取得
         titleView = findViewById(R.id.etTitle);
         contentView = findViewById(R.id.etContent);
-
 
         // Intentからメモの情報を取得
         Intent intent = getIntent();
@@ -45,5 +48,15 @@ public class DetailActivity extends AppCompatActivity {
         // メモの情報を表示
         titleView.setText(title);
         contentView.setText(content);
+    }
+
+    // 戻るボタンを押したときの反応を定義
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // この画面を閉じて前の画面に戻る
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
